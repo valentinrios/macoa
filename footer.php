fcf04cdb80e596822fe653695f8f1607cf83a7ec
Valentin Rios <v@valentn.com>
2018-04-02 18:29:44 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <footer>
+      <p>
+        This is my footer
+      </p>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+  </body>
+</html>
